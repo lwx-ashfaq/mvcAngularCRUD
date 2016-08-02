1034a5caf1e13b0450fd86def565b34f71fe1f34
rahul868 <raval.rahul868@gmail.com>
2016-08-02 10:13:55 +0530

    Add files via upload

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using AngularJSDemo4.Filter;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace AngularJSDemo4
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Filters.Add(new ValidateModelAttribute());
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApiWithAction",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
